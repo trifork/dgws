8c9defecafcd964e4c248b24652483baa38aead4
Martin Westergaard Lassen <mwl@trifork.com>
2012-12-10 07:32:32 +0100

    Implemented ValueResolverAware

--- dgws-spring/src/main/java/com/trifork/dgws/annotations/DgwsConfiguration.java
+++ dgws-spring/src/main/java/com/trifork/dgws/annotations/DgwsConfiguration.java
@@ -1,44 +1,39 @@
 package com.trifork.dgws.annotations;
 
 import com.trifork.dgws.sosi.SOSISecurityInterceptor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportAware;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.EmbeddedValueResolverAware;
+import org.springframework.context.annotation.*;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.StringValueResolver;
 
 import java.util.Map;
 
 @Configuration
 @ImportResource("classpath:/dk/trifork/dgws/dgws-protection.xml")
-public class DgwsConfiguration implements ImportAware {
-    Boolean production;
-    Boolean skipSosi;
+public class DgwsConfiguration implements ImportAware, EmbeddedValueResolverAware {
+    Boolean production = true;
+    Boolean skipSosi = false;
+    private StringValueResolver resolver;
 
     public void setImportMetadata(AnnotationMetadata importMetadata) {
         final Map<String, Object> meta = importMetadata.getAnnotationAttributes("com.trifork.dgws.annotations.EnableDgwsProtection");
-        production = Boolean.parseBoolean(meta.get("production").toString());
-        skipSosi = Boolean.parseBoolean(meta.get("skipSOSI").toString());
+        production = resolveBoolean(meta.get("production"));
+        skipSosi = resolveBoolean(meta.get("skipSOSI"));
+    }
+
+    private boolean resolveBoolean(Object value) {
+        return Boolean.parseBoolean(resolver.resolveStringValue(value.toString()));
     }
 
     @Bean
     public SOSISecurityInterceptor sosiSecurityInterceptor() {
         SOSISecurityInterceptor interceptor = new SOSISecurityInterceptor();
-        if (production != null && production.booleanValue()) {
-            interceptor.setProduction(true);
-        }
-        else {
-            interceptor.setProduction(false);
-        }
-
-        if (skipSosi != null && skipSosi.booleanValue()) {
-            interceptor.setCanSkipSosi(true);
-        }
-        else {
-            interceptor.setCanSkipSosi(false);
-        }
+        interceptor.setProduction(production);
+        interceptor.setCanSkipSosi(skipSosi);
         return interceptor;
+    }
 
-
+    public void setEmbeddedValueResolver(StringValueResolver resolver) {
+        this.resolver = resolver;
     }
 }
